12f24a19c32aebf8068bb7e1afc1c05e1cd2bad9
alex289 <mccr4fthd@gmail.com>
2022-09-16 22:53:06 +0200

    Use image component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import image from "@astrojs/image";
 import vercel from '@juanm04/astrojs-vercel/edge';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  integrations: [image()]
 });
