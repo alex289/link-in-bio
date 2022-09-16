3d37971cc198ecb649032cd5139a431b366b8e38
alex289 <mccr4fthd@gmail.com>
2022-09-16 22:54:20 +0200

    Revert "Use image component"
    This reverts commit 12f24a19c32aebf8068bb7e1afc1c05e1cd2bad9.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import image from "@astrojs/image";
 import vercel from '@juanm04/astrojs-vercel/edge';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [image()]
 });
