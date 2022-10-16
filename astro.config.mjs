1d51bb3df1aea08f7fb2e97d87ba3542341da65b
alex289 <mccr4fthd@gmail.com>
2022-10-16 11:23:46 +0200

    refactor: Use astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@juanm04/astrojs-vercel/edge';
+import vercel from "@astrojs/vercel/edge";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
