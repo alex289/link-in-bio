2cc37cc280e290dbc5b7fda8fd0c1ea2d71a4bb8
alex289 <mccr4fthd@gmail.com>
2022-10-16 11:43:06 +0200

    feat: Deploy on the edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   adapter: vercel()
 });
