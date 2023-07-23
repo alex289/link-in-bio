019d05502c8f5e11c9e4f8b8172f17c38cedf94c
alex289 <mccr4fthd@gmail.com>
2023-07-23 17:24:20 +0200

    Format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,17 @@
 module.exports = {
-    extends: [
-      "plugin:astro/recommended",
-    ],
-    overrides: [
-      {
-        files: ["*.astro"],
-        parser: "astro-eslint-parser",
-        parserOptions: {
-          parser: "@typescript-eslint/parser",
-          extraFileExtensions: [".astro"],
-        },
-        rules: {
-          // override/add rules settings here, such as:
-          // "astro/no-set-html-directive": "error"
-        },
+  extends: ['plugin:astro/recommended'],
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
-    ],
-  }
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
+    },
+  ],
+};
