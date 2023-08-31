a3d35496df8529b118ef3056fbf6a9fd23aa85bf
alex289 <mccr4fthd@gmail.com>
2023-08-31 08:18:00 +0200

    feat: Astro v3

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    edgeMiddleware: true,
+  }),
 });
