62324a4d0777f1c4be297651244951bb30cbf270
alex289 <mccr4fthd@gmail.com>
2024-10-02 19:43:56 +0200

    fix: Switch back to serverless

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
